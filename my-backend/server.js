27bf36368f2a2223c3232a1948d2e8abaf726590
Affan Nabeel <affan@technassolution.com>
2026-02-13 15:13:22 +0530

    fix port for render

--- my-backend/server.js
+++ my-backend/server.js
@@ -13,7 +13,7 @@ app.get("/api/data", (req, res) => {
     res.json({ name: "Your Name", project: "Free Hosting Project" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
